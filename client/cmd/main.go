09a3fc6ad288b7920527c57280742c0ff8231b2a
软件工程师李飛 <lifei@7v1.net>
2023-10-24 11:46:01 +0800

    Print config after binding cmd flags

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -50,14 +50,14 @@ Find more quic-tun information at:
 	return rootCmd
 }
 
-func runCommand(cmd *cobra.Command, args []string) error {
+func runCommand(cmd *cobra.Command, args []string) error {	
 	options.PrintWorkingDir()
 	options.PrintFlags(cmd.Flags())
-	options.PrintConfig()
 
 	if err := viper.BindPFlags(cmd.Flags()); err != nil {
 		return err
-	}
+	}	
+	options.PrintConfig()
 
 	if err := viper.Unmarshal(clientOptions); err != nil {
 		return err
